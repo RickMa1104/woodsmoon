2701a817de45282a97dffa6ec2383f4376bd400b
ClarkSeeMa <38975677+ClarkSeeMa@users.noreply.github.com>
2019-01-01 15:48:24 +0800

    f

---
+++ pages/PHP.php
@@ -0,0 +1,19 @@
+<?php
+// echo "test echo";
+// print ('test print');
+// printf ("test printf %d,%f",12,12.3);
+// $arr =["test","print_r"];
+// print_r($arr);
+// $td ="test var_dump";
+// var_dump($td);
+// define("def","test define");
+// echo def;
+$arr2=["arr[0]","arr[1]"];
+echo $arr2[0];
+$arr3=["key1"=>"arr[key1]","key2"=>"arr[key2]"];
+echo $arr3[key1];
+$a1="conn";
+$a2="ection";
+echo $a1.$a2;
+?>
+
